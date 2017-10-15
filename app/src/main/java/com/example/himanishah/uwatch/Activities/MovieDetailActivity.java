41df5e2e28bd8fcc4fa32681cb535d8af54934e3
Himani Shah <hjshah2@asu.edu>
2017-10-15 15:24:49 -0700

    Views changed for tablet layout

--- app/src/main/java/com/example/himanishah/uwatch/Activities/MovieDetailActivity.java
+++ app/src/main/java/com/example/himanishah/uwatch/Activities/MovieDetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.himanishah.uwatch.Activities;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,21 @@ import com.example.himanishah.uwatch.R;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
+    private Bundle bundle;
+    private Fragment movieDetail;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+
+        //Creating Fragment MovieDetailFragment
+        bundle = getIntent().getExtras();
+        movieDetail = new MovieDetailFragment();
+        movieDetail.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.movie_detail_container, movieDetail)
+                .commit();
     }
 }
+
